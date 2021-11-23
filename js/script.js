5fe38b82c2e18c9f68dbc8882f47d4fe91daef7c
ArfI Idris <idrisarfi@gmail.com>
2021-11-23 15:27:36 +0300

    update readme.md

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-
 $(document).ready(function() {
-  $(".design").click(function() {
-    $("#design, .design").toggle("slow");
-  })
-});
-$("#design").click(function() {
-  $(".design, #design").toggle("slow");
-});
-$(".development").click(function() {
-  $("#development, .development").toggle("slow");
-});
-$("#development").click(function() {
-  $(".development, #development").toggle("slow");
-});
+  
+})
